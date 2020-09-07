7e3cdab6c04cc6a81538ee109ca32e73d15a0abd
Noah Schutte <noahschutte@noahs-mbp.lan>
2020-09-07 14:12:33 -0600

    run local development ngrok server

---
+++ purchase-slash-command.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/purchase', async (req, res) => {
+    console.log(req.body)
+    res.send('OK')
+})
+
+const PORT = 9647
+app.listen(PORT, () => {
+    console.log(`ddslackbot has started on port: ${PORT}`)
+})
